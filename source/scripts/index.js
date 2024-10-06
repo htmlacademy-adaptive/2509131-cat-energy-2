e30aeafc81edf36b77615f7117988d426444c8dd
Екатерина Машкина <51336273+Katerina2391@users.noreply.github.com>
2024-10-06 17:04:46 +0300

    Исправляет поведение при отключенном js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,9 +1,15 @@
 const nav = document.querySelector('.nav');
 const Toggle = document.querySelector('.main-header__toggle');
 const mainHeader = document.querySelector('.main-header');
+const mapWrapper = document.querySelector('.contacts__map-wrapper');
 
 mainHeader.classList.remove('main-header--no-js');
+Toggle.classList.remove('main-header__toggle--close');
+Toggle.classList.add('main-header__toggle--open');
 nav.classList.remove('nav--no-js');
+nav.classList.remove('nav--is-opened');
+nav.classList.add('nav--is-closed');
+mapWrapper.classList.remove('contacts__map-wrapper--no-js');
 
 Toggle.addEventListener('click', () => {
   if (nav.classList.contains('nav--is-closed')) {
